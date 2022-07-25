30fd88c27a5cb20ae3bbc30ebd377b38a88b1102
yousuf122786 <yousuf122786@gmail.com>
2022-07-25 19:12:55 +0000

    There was a bug in fsm implemtation in verilog 1)For the input 11011 the actual output is 1 but the output for the code is 0 as there is a bug in the code. 2)For the input 1011011 the output should be 1 as it is overlapping but the output is 0 as there is bug in the code.

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -23,4 +23,54 @@ async def test_seq_bug1(dut):
     dut.reset.value = 0
     await FallingEdge(dut.clk)
 
+    dut.inp_bit.value = 1
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value = 1
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value = 0
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value = 1
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value = 1
+    await FallingEdge(dut.clk)
+    assert dut.seq_seen.value == 1
+
     cocotb.log.info('#### CTB: Develop your test here! ######')
+
+
+
+@cocotb.test()
+async def test_seq_bug2(dut):
+    """Test for seq detection """
+
+    clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
+    cocotb.start_soon(clock.start())        # Start the clock
+
+    # reset
+    dut.reset.value = 1
+    await FallingEdge(dut.clk)  
+    dut.reset.value = 0
+    await FallingEdge(dut.clk)
+
+    dut.inp_bit.value = 1
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value = 0
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value = 1
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value = 1
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value = 0
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value = 1
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value = 1
+    await FallingEdge(dut.clk)
+
+    assert dut.seq_seen.value == 1
+
+    cocotb.log.info('#### CTB: Develop your test here! ######')
+
+
+
+
